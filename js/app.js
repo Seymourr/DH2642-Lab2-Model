0fd659edb2eb4ef18db2fabe9ba16f1a176fe6df
augustjanse <augustj@kth.se>
2016-02-09 13:57:53 +0100

    Instantiate views

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ $(function() {
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"));
-
+	var splashView = new SplashView($("#view1"));
+	var sidebar = new SidebarView($("#view2"), model);
+	var selectDishView = new SelectDishView($("#view3"), model);
+	var dishDetailsView = new DishDetailsView($("#view4"), model);
+	var dinnerHeader = new DinnerHeaderView($("#view5"));
+	var overviewView = new OverviewView($("#view6"), model);
+	var instructionsView = new InstructionsView($("#view7"), model);
 });
