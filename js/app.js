e26228fcb26000aa638cf1154e20c3b2833805ae
augustjanse <augustj@kth.se>
2016-02-16 15:48:58 +0100

    Fix instructions view

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@ $(function() {
 
 	// Example values for model
 	model.setNumberOfGuests(7);
-	model.addDishToMenu(2);
+	model.addDishToMenu(3);
 	model.addDishToMenu(100);
 	model.addDishToMenu(201);
 
 	//And create the needed controllers and views
 	//var splashView = new SplashView($("#view1"));
-	var sidebar = new SidebarView($("#view2"), model);
+	//var sidebar = new SidebarView($("#view2"), model);
 	//var selectDishView = new SelectDishView($("#view3"), model);
-	var dishDetailsView = new DishDetailsView($("#view4"), model);
-	//var dinnerHeader = new DinnerHeaderView($("#view5"), model);
+	//var dishDetailsView = new DishDetailsView($("#view4"), model);
+	var dinnerHeader = new DinnerHeaderView($("#view5"), model);
 	//var overviewView = new OverviewView($("#view6"), model);
-	//var instructionsView = new InstructionsView($("#view7"), model);
+	var instructionsView = new InstructionsView($("#view7"), model);
 });
