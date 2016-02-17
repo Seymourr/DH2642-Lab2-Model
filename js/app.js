bee1e61ec78b1f5983717775fdb41619a7cf6e29
augustjanse <augustj@kth.se>
2016-02-17 10:23:38 +0100

    Fix traversing

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(function() {
 	model.addDishToMenu(201);
 
 	//And create the needed controllers and views
-	//var splashView = new SplashView($("#view1"), this);
+	var splashView = new SplashView($("#view1"), this);
 	var sidebar = new SidebarView($("#view2"), model);
 	var sideBarController = new SidebarController(sidebar, model, $("#view2"), this);
 	var selectDishView = new SelectDishView($("#view3"), model);
@@ -24,7 +24,7 @@ $(function() {
 	var overviewController = new OverviewController(overviewView, model, this);
 	var instructionsView = new InstructionsView($("#view7"), model);
 
-	views.push(sidebar, selectDishView, dishDetailsView, dinnerHeader, overviewView);
+	views.push(splashView, sidebar, selectDishView, dishDetailsView, dinnerHeader, overviewView, instructionsView);
 
 	// dish may be undefined
 	this.goForward = function(dish){
@@ -42,6 +42,16 @@ $(function() {
 			views[i].hide();
 		}
 
+		if (currentView === 1 || currentView === 4) {
+			// bump to next number that will result in a full view
+			currentView++;
+		}
+
+		if (currentView === 2 || currentView === 3) {
+			views[1].show(); // also show sidebar
+		} else if (currentView === 5 || currentView === 6) {
+			views[4].show(); // also show subheader
+		}
 		views[currentView].show(dish);
 	}
 });
